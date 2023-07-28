643ce39a7934851613a492c4532950f3f1dc9180
Hostnomics <hostnomics@gmail.com>
2023-07-27 23:49:27 -0400

    Fixed CSS issue with textarea and text field. Added dynamic submit button to components/Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -17,41 +17,49 @@ const Form = ({  type, post, setPost, submitting, handleSubmit }) => {
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
               <label>
-                <span className="font-satoshi font-semibold text-base text-gray-700">
-                  Your AI Prompt
-                </span>
-<br></br>
-                <textarea 
-                  value={post.prompt}
-                  onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-                  placeholder="Write your prompt here..."
-                  required
-                  className="from_textarea w-full"
-                  
-                />
+                  <span className="font-satoshi font-semibold text-base text-gray-700">
+                    Your AI Prompt
+                  </span>
+  {/* <br></br> */}
+                  <textarea 
+                    value={post.prompt}
+                    onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+                    placeholder="Write your prompt here..."
+                    required
+                    className="form_textarea "
+                    
+                  />
               </label>
 
 
               <label>
-                  <span className="font-satoshi font-semibold text-base text-gray-700">
-                    Tag
-                    <span className="font-normal">(#product, #webdevelopment, #idea)</span>
-                  </span>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">
+                      Tag {` `}
+                      <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+                    </span>
 
-                  <input
-                    value={post.tag}
-                    onChange={(e) => setPost({ ...post, tag: e.target.value })}
-                    type='text'
-                    placeholder="Add a #tag"
-                    required
-                    className="from_input"
-                  />
+                    <input
+                      value={post.tag}
+                      onChange={(e) => setPost({ ...post, tag: e.target.value })}
+                      type='text'
+                      placeholder="Add a #tag"
+                      required
+                      className="form_input"
+                    />
               </label>
 
               <div className="flex-end mx-3 mb-5 gap-4">
-                <Link href="/" >
-                  Cancel
-                </Link>
+                      <Link href="/" className="text-gray-500 text-sm">
+                        Cancel
+                      </Link>
+
+                      <button
+                        type='submit'
+                        disabled={submitting}
+                        className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+                      >
+                        {submitting ? `${type}ing...` : type}
+                      </button>
               </div>
       </form>
 
