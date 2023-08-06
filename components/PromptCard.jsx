59877906dd8848b964d35842019e35d30a9cf3a8
hostnomics <hostnomics@gmail.com>
2023-08-05 23:27:58 -0400

    Added copy button for user to copy a prompt and styling for the prompt text display and tag display to components/PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -14,6 +14,9 @@ import {usePathname, useRouter} from 'next/navigation';
 // bring in some PROPS (2:23:20)
 const PromptCard = ( { post, handleTagClick, handleEdit, handleDelete }) => {
 
+//Added (2:27:26) copied state to copy a prompt
+  const [copied, setCopied] = useState("");
+
   const {data: session } = useSession()
   return (
     <div className="prompt_card">
@@ -37,10 +40,20 @@ const PromptCard = ( { post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </h3>
         </div>
-
-      </div>
-
+{/* 2:27:10-ish Added Copy Prompt button */}
+        <div className="copy_btn" onClick={() => {}}>
+          <Image 
+            src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
+            width={12}
+            height={12}
+          />
+        </div>
     </div>
+{/* (2:28:21)- p tags for prompt and tag */}
+          <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+          <p className="font-inter text-sm blue_gradient cursor-pointer">{post.tag}</p>  
+         
+  </div>
   )
 } 
 
